10fde4558f4e9a6a030fdbc2fb1ed1e40449701f
codetopaper <72480249+codetopaper@users.noreply.github.com>
2022-12-11 14:37:31 +0800

    Add files via upload

---
+++ train_clothing1m.py
@@ -0,0 +1,430 @@
+
+import os
+import numpy as np
+from numpy import inf
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.models as models 
+import csv
+import train_clothing1m_dataloader as dataloader
+from torch.autograd import Variable
+
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--folder_log', default = 'log', required=False, type=str, help="The directory to save logs and checkpoints.")
+parser.add_argument('--data_path', type=str, default='./Clothing1M/images', help="The path to dataset Clothing1M.")
+parser.add_argument('--batch_size', type=int, default=32)
+parser.add_argument('--num_workers', type=int,default=4, help="Number of parallel workers to parse dataset.")
+parser.add_argument('--seed', type=int, default=1, help="Random seed to be used.")
+parser.add_argument('--mixup_alpha', type=float, default=0.5, help = 'Mixup hyperparameter alpha.')
+parser.add_argument('--avg_prediction_vector', type=str, default=None, help='The path to load averaged prediction vector. If not given, would obtain it by averaging the individual prediction vectors in args.idv_prediction_vectors.')
+parser.add_argument('--idv_prediction_vectors', nargs='+', default=[], help='The path to load individual prediction vectors. If not given, the individual prediction vectors would be obtained from args.idv_weights.')
+parser.add_argument('--idv_weights', nargs='+', default=[], help='The path to load individual model weights. If not given, models need to train on the 50k clean set first to obtained weights.')
+parser.add_argument('--normalized_class_ratio', type=str, default='./clothing1m_clean_trainreverseclassratio.txt', help='The normalized class ratio vector in txt format.')
+parser.add_argument('--beta', type=float, default=0.03, help='If the entry value in a prediction vector is < 1/args.num_classes-args.beta, its value would be 0.')
+parser.add_argument('--alpha', type=float, default=1.05, help='The threshold for NC instances, if the instance has (alpha, beta)-generalized KL divergence value >0, it is an NC instance.')
+parser.add_argument('--nc_set', type=str, default=None, help='The .txt file of all identified NC instances.')
+parser.add_argument('--no_vectors', type=int, default=20, help='The number of uniform-like vectors to identify NC instances.')
+parser.add_argument('--sigma', type=float, default=0.05, help='The standard deviation used to generate uniform-like vectors.')
+parser.add_argument('--omega_c', type=float, default=1.0, help='The weightage for clean instances.')
+parser.add_argument('--omega_nnc', type=float, default=32, help='The weightage for non-NC instances.')
+parser.add_argument('--omega_nc', type=float, default=1.0, help='The weightage for NC instances.')
+parser.add_argument('--epochs', required=False, type=int, default=20)
+parser.add_argument('--lr', type=float, default=0.001)
+parser.add_argument('--momentum', type=float, default=0.9)
+parser.add_argument('--weight_decay', type=float, default=1e-3)
+parser.add_argument('--finetune_epochs', type=int, default=25)
+parser.add_argument('--num_classes', required=False, type=int, default=14)
+
+args = parser.parse_args()
+print(args)
+
+
+
+def create_folder(folder):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+
+
+def build_model(device):
+    net = models.resnet50(pretrained=True)
+    net.fc = nn.Linear(2048, args.num_classes)
+    net = net.to(device)
+    return net
+
+
+
+def prediction_vector(net, dataloader, name):
+    '''
+    Loads model weights, and produce softmax vectors for the instances
+    '''
+    probs = []
+    net.eval()
+    with torch.no_grad():
+        for (inputs, targets) in dataloader:
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = net(inputs)
+            pred = F.softmax(outputs, dim=1)
+            probs.extend(list(pred.data.cpu().numpy()))
+    probs = np.array(probs, dtype=np.float32)
+    np.save(log_dir + '/' + name + '.npy', probs)
+
+    return (log_dir + '/' + name + '.npy')
+
+
+
+def prediction_avg(args, dirs, name):
+    '''
+    dirs: a list of prediction softmax vectors for the complete noisy training set, eg: ['xxx.npy', 'yyy.npy', 'zzz.npy']
+    Average the list of predictions in dirs and save it as '{}/{}_soft_labels.npy'.format(log_dir,name)
+    '''
+
+    train_length = len(np.load(dirs[0]))
+    prediction = np.zeros((train_length, len(dirs), args.num_classes), dtype=np.float32)
+    for idx in range(len(dirs)):
+        results = np.load(dirs[idx])
+        prediction[:, idx] = results
+    avg = prediction.mean(axis=1)
+    np.save('{}/{}.npy'.format(log_dir, name), avg)
+    return '{}/{}.npy'.format(log_dir, name)
+
+
+
+def identify_nc(args):
+    # generate uniform-like vectors
+    np.random.seed(args.seed)
+    uniform_vectors = np.random.normal(1.0 / args.num_classes, args.sigma, (args.no_vectors, args.num_classes))
+    # row normalization
+    uniform_vectors[uniform_vectors < 0] = 0.005
+    uniform_vectors[uniform_vectors > 1.0] = 1.0
+    row_sums = uniform_vectors.sum(axis=1)
+    uniform_vectors /= row_sums[:, np.newaxis]
+
+    # compute the first term in (alpha, beta)-generalized kl divergence
+    first_term = args.alpha * (np.sum(uniform_vectors * np.log2(uniform_vectors), axis=1))
+    first_term = np.repeat(first_term[:, np.newaxis], np.load(args.avg_prediction_vector).shape[0], axis=1)
+
+    # set threshold to delete very small entries in the averaged prediction vector
+    deletion = 1.0 / float(args.num_classes) - args.beta
+    assert deletion > 0
+    temp = np.load(args.avg_prediction_vector)
+    temp[temp < deletion] = 0  # delete very small entries
+
+    # compute the second term in (alpha, beta)-generalized kl divergence
+    temp = np.log2(temp)
+    temp[temp == -inf] = 0.0
+    second_term = 1.0 * uniform_vectors[:, np.newaxis] * temp
+    second_term = np.sum(second_term, axis=-1)
+
+    judgement = first_term >= second_term
+    indices = np.sum(judgement, axis=0)  # sum over the votes from all uniform-like vectors
+    with open('%s/noisy_train_key_list.txt' % args.data_path, 'r') as f:
+        lines = f.read().splitlines()
+    nc_set = [lines[i] for i in range(len(indices)) if
+              indices[i] > 0.0]  # as long as there is a vote from any uniform-like vectors, this instance is NC
+    nc_set = set(nc_set)
+    for l in nc_set:
+        with open(log_dir + '/nc_set.txt', 'a') as the_file:
+            the_file.write(l.strip() + '\n')
+    if len(nc_set) == 0:
+        with open(log_dir + '/nc_set.txt', 'a') as the_file:
+            the_file.write('empty_set\n')
+
+    return log_dir + '/nc_set.txt'
+
+
+
+def mixup_data(x, y, alpha=1.0, use_cuda=True):
+    '''Returns mixed inputs, pairs of targets, and lambda'''
+    if alpha > 0:
+        lam = np.random.beta(alpha, alpha)
+        lam = max(lam, 1-lam)
+    else:
+        lam = 1
+
+    batch_size = x.size()[0]
+    if use_cuda:
+        index = torch.randperm(batch_size).cuda()
+    else:
+        index = torch.randperm(batch_size)
+
+    mixed_x = lam * x + (1 - lam) * x[index, :]
+    y_a, y_b = y, y[index]
+    return mixed_x, y_a, y_b, lam
+
+
+
+def mixup_criterion(criterion, pred, y_a, y_b, lam):
+    return lam * criterion(pred, y_a) + (1 - lam) * criterion(pred, y_b)    
+
+
+
+def train(optimizer, ratio):
+
+    criterion = nn.CrossEntropyLoss()
+
+    def ce(input, target):
+        return torch.mean(-torch.sum(target * torch.log(input), 1))
+
+    loss_nnc_ = 0.0
+    loss_c_ = 0.0
+    loss_nc_ = 0.0
+
+    net.train()
+    dataloader_iterator = iter(train_clean_loader)
+    for batch_idx, data1 in enumerate(train_loader):
+        data, target, target_hard = data1[0].numpy(), data1[1].numpy(), data1[2].numpy()
+
+
+        #nc instances
+        nc_indices = target_hard > (args.num_classes - 1)
+        nc_len = np.sum(nc_indices)
+        if nc_len > 0:
+            data_nc, target_nc = data[nc_indices], target[nc_indices]
+            data_nc, target_nc = Variable(torch.FloatTensor(data_nc).cuda()), Variable(torch.FloatTensor(target_nc).cuda())
+            output_nc = net(data_nc)
+            # equation (7)
+            loss_nc = ce(F.softmax(output_nc, dim=1), target_nc)
+            loss_nc_ += loss_nc.item()
+        else:
+            loss_nc = 0.0
+
+
+        #non-NC instances
+        nnc_indices = target_hard < args.num_classes
+        nnc_len = np.sum(nnc_indices)
+        if nnc_len > 0:
+            data_nnc, target_nnc, target_nnc_hard = data[nnc_indices], target[nnc_indices], target_hard[nnc_indices]
+            data_nnc, target_nnc, target_nnc_hard = Variable(torch.FloatTensor(data_nnc).cuda()), Variable(torch.FloatTensor(target_nnc).cuda()), Variable(torch.from_numpy(target_nnc_hard).long().cuda())
+
+            #apply mixup
+            data_nnc, target_nnc_a, target_nnc_b, lam = mixup_data(data_nnc, target_nnc, args.mixup_alpha)
+            #the normalized class ratio
+            pre1 = ratio[torch.cuda.LongTensor(target_nnc_hard.data)]
+            #double-hot vector
+            target_nnc_a += pre1
+            target_nnc_b += pre1
+        
+            data_nnc, target_nnc_a, target_nnc_b = map(Variable, (data_nnc, target_nnc_a, target_nnc_b))
+
+            output = net(data_nnc)
+
+            #equation (6)
+            pre_a = torch.mul(F.softmax(output, dim=1), target_nnc_a)
+            loss_a = -lam*(torch.log(pre_a.sum(1))).sum(0)
+        
+            pre_b = torch.mul(F.softmax(output, dim=1), target_nnc_b)
+            loss_b = -(1 - lam)*(torch.log(pre_b.sum(1))).sum(0)
+
+            loss_nnc = loss_a + loss_b
+            loss_nnc /= float(nnc_len)
+            loss_nnc_ += loss_nnc.item()
+        else:
+            loss_nnc = 0.0
+
+
+        #clean instances
+        try:
+            data2 = next(dataloader_iterator)
+        except StopIteration:
+            dataloader_iterator = iter(train_clean_loader)
+            data2 = next(dataloader_iterator)
+
+        data_c, target_c = data2[0].cuda(), data2[1].cuda()
+        data_c, target_c_a, target_c_b, lam_c = mixup_data(data_c, target_c, args.mixup_alpha)
+        data_c, target_c_a, target_c_b = map(Variable, (data_c, target_c_a, target_c_b))
+
+        output_c = net(data_c)
+        loss_c = mixup_criterion(criterion, output_c, target_c_a, target_c_b, lam_c)
+        loss_c_ += loss_c.item()
+
+
+
+        # backward
+        optimizer.zero_grad()
+        loss = (args.omega_c*loss_c + args.omega_nnc*loss_nnc + args.omega_nc*loss_nc)/args.batch_size
+        loss.backward()
+        optimizer.step()
+
+    return loss_c_+loss_nnc_+loss_nc_
+
+
+
+def test(net, dataloader):
+
+    criterion = nn.CrossEntropyLoss()
+
+    count = 0.0
+    correct = 0.0
+    net.eval()
+    with torch.no_grad():
+        for (inputs, targets) in dataloader:
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = net(inputs)
+            loss = criterion(outputs, targets)
+            _, predicted = torch.max(outputs.data, 1)
+            correct += predicted.eq(targets.data).cpu().sum().item()
+            count += len(inputs)
+    return correct/count, loss.item()/count
+
+
+
+def finetune(optimizer):
+
+    loss_avg = 0.0
+    criterion = nn.CrossEntropyLoss()
+
+    net.train()
+    for batch_idx, (data, target) in enumerate(train_clean_loader):
+        data, target = data.cuda(), target.cuda()
+
+        output = net(data)
+        loss = criterion(output, target)
+        loss /= float(len(data))
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    return loss.item()
+
+
+
+
+def main(args):
+
+    #the normalized class ratio term in the double-hot vector is obtained from args.normalized_class_ratio
+    file = open(args.normalized_class_ratio, "r")
+    f = file.readlines()
+    ratio = np.zeros((args.num_classes, args.num_classes))
+    for i in range(args.num_classes):
+        ratio[i] = f[i].split()
+    file.close()
+    ratio = np.array(ratio, dtype=np.float32)
+    ratio = torch.from_numpy(ratio)
+    ratio = ratio.requires_grad_(False).cuda()
+
+
+    optimizer = torch.optim.SGD(net.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay, nesterov=True)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=3)
+    train_bvacc = 0.0
+
+    #logging
+    with open(log_dir+'/'+'record.csv', 'a', newline='') as csvFile:
+        writer = csv.writer(csvFile)
+        writer.writerow(np.array(['epoch', 'train loss', 'val_loss', 'val_acc', 'test acc']))
+    csvFile.close()
+
+    print('\nNow training\n')
+
+
+    #training with double-hot vector
+    for epoch in range(args.epochs):
+            
+        train_loss = train(optimizer, ratio)
+        val_acc, val_loss = test(net, val_loader)
+        test_acc, _ = test(net, test_loader)
+
+        if train_bvacc < val_acc:
+            train_bvacc = val_acc
+            with open(log_dir+'/'+'record.csv', 'a', newline='') as csvFile:
+                writer = csv.writer(csvFile)
+                writer.writerow(np.array([epoch, train_loss, val_loss, val_acc, test_acc, 'best val acc']))
+            csvFile.close()
+            torch.save(net.state_dict(), log_dir + '/train_best.pth')
+            
+        else:
+            with open(log_dir+'/'+'record.csv', 'a', newline='') as csvFile:
+                writer = csv.writer(csvFile) 
+                writer.writerow(np.array([epoch, train_loss, val_loss, val_acc, test_acc]))
+            csvFile.close()
+        
+        scheduler.step(val_loss)
+     
+
+
+    #finetuning
+    with open(log_dir+'/'+'record.csv', 'a', newline='') as csvFile:
+        writer = csv.writer(csvFile)
+        writer.writerow(np.array(['epoch', 'train loss', 'val_loss', 'val_acc','test acc']))
+    csvFile.close()
+  
+    print('\nNow finetuning\n')
+
+    net.load_state_dict(torch.load(log_dir + '/train_best.pth'))
+
+    optimizer = torch.optim.Adam(net.parameters(), lr=0.0000005, weight_decay=1e-3)
+    finetune_bvacc = 0.0
+    for epoch in range(args.finetune_epochs):
+            
+        train_loss = finetune(optimizer)
+        val_acc, val_loss = test(net, val_loader)
+        test_acc, _ = test(net, test_loader)
+
+        if finetune_bvacc < val_acc:
+            finetune_bvacc = val_acc
+            with open(log_dir+'/'+'record.csv', 'a', newline='') as csvFile:
+                writer = csv.writer(csvFile)
+                writer.writerow(np.array([epoch, train_loss, val_loss, val_acc, test_acc, 'best val acc']))
+            csvFile.close()
+            torch.save(net.state_dict(), log_dir + '/finetune_best.pth')
+        else:
+            with open(log_dir+'/'+'record.csv', 'a', newline='') as csvFile:
+                writer = csv.writer(csvFile) 
+                writer.writerow(np.array([epoch, train_loss, val_loss, val_acc, test_acc]))
+            csvFile.close()
+    
+
+
+if __name__ == "__main__":
+
+    torch.manual_seed(args.seed)
+    np.random.seed(args.seed)
+
+    #logging
+    log_dir = args.folder_log + '/' +str(args.seed)
+    create_folder(log_dir)
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    torch.backends.cudnn.benchmark = True
+
+    #obtain the averaged prediction vectors, which would be used to compute NC instances and double-hot vectors later
+    net = build_model(device)
+    loader = dataloader.clothing_dataloader(root=args.data_path, batch_size=args.batch_size, num_workers=args.num_workers)
+    if not args.avg_prediction_vector:
+        if not args.idv_prediction_vectors:
+            if not args.idv_weights:
+                assert "Please train models first!"
+            train_val_mode_loader, train_length = loader.run('train_val_mode')
+            args.idv_prediction_vectors = []
+            count = 0
+            for i in args.idv_weights:
+                net.load_state_dict(torch.load(i))   
+                name_ = i.split('/')[-1].split('.pth')[0] + str(count)
+                idv_prediction_vector = prediction_vector(net, train_val_mode_loader, name_)
+                args.idv_prediction_vectors.append(idv_prediction_vector)
+                count += 1
+                 
+        args.avg_prediction_vector = prediction_avg(args, args.idv_prediction_vectors, name='avg_prediction')
+        
+    #identify NC instances
+    if not args.nc_set:
+        args.nc_set = identify_nc(args)
+        
+    #dataloader initialization
+    loader = dataloader.clothing_dataloader(root=args.data_path, batch_size=args.batch_size, avg_prediction_vector=args.avg_prediction_vector, nc_set=args.nc_set, num_workers=args.num_workers)
+    train_loader = loader.run('nc_training')
+    train_clean_loader = loader.run('train_clean')
+    val_loader = loader.run('val')
+    test_loader = loader.run('test')
+
+    #model initialization
+    net = build_model(device)
+    
+    main(args)
+
+
---
+++ train_clothing1m_dataloader.py
@@ -0,0 +1,233 @@
+from torch.utils.data import Dataset, DataLoader
+import torchvision.transforms as transforms
+import random
+import numpy as np
+from PIL import Image
+import json
+import torch
+import math
+
+
+class clothing_dataset(Dataset):
+    def __init__(self, root, transform, mode, avg_prediction_vector=None, nc_set=None, num_class=14):
+        
+        self.root=root
+        self.avg_prediction_vector=avg_prediction_vector
+        self.nc_set=nc_set
+        self.transform=transform
+        self.mode=mode
+        self.train_labels={}
+        self.test_labels={}
+        self.val_labels={}
+        self.num_class = num_class
+
+
+        with open('%s/noisy_label_kv.txt'%self.root, 'r') as f:
+            lines = f.read().splitlines()
+            for l in lines:
+                entry = l.split()
+                img_path = '%s/'%self.root+entry[0][7:]
+                self.train_labels[img_path] = int(entry[1])
+        
+        with open('%s/clean_label_kv.txt'%self.root, 'r') as f:
+            lines = f.read().splitlines()
+            for l in lines:
+                entry = l.split()
+                img_path = '%s/'%self.root+entry[0][7:]
+                self.test_labels[img_path] = int(entry[1])
+                
+        if mode == 'test':
+            self.test_imgs = []
+            with open('%s/clean_test_key_list.txt'%self.root,'r') as f: #####
+                lines = f.read().splitlines()
+                for l in lines:
+                    img_path = '%s/'%self.root+l[7:]
+                    self.test_imgs.append(img_path)
+
+        elif mode == 'val':
+            self.val_imgs = []
+            with open('%s/clean_val_key_list.txt'%self.root, 'r') as f: #####
+                lines = f.read().splitlines()
+                for l in lines:
+                    img_path = '%s/'%self.root+l[7:]
+                    self.val_imgs.append(img_path)
+
+        elif mode == 'train_clean':
+            self.train_clean_imgs = []
+            with open('%s/clean_train_key_list.txt'%self.root,'r') as f:#####
+                lines = f.read().splitlines()
+                for l in lines:
+                    img_path = '%s/'%self.root+l[7:]
+                    self.train_clean_imgs.append(img_path)
+                    
+        elif mode == 'nc_training':
+            self.soft_labels = np.load(self.avg_prediction_vector)
+            self.nc_imgs = []
+            with open('%s'%self.nc_set,'r') as f:#
+                lines = f.read().splitlines()
+            for l in lines:
+                img_path = '%s/'%self.root+l[7:]
+                self.nc_imgs.append(img_path)
+                
+            uniform_vector = np.ones(self.num_class)/float(self.num_class)
+            uniform_vector = np.array(uniform_vector, dtype=np.float32)
+            
+            self.train_imgs = []            
+            with open('%s/noisy_train_key_list.txt'%self.root,'r') as f:#
+                lines = f.read().splitlines()
+            for index, l in enumerate(lines):
+                img_path = '%s/'%self.root+l[7:]
+                self.train_imgs.append(img_path)
+                if l in self.nc_imgs:
+                    self.soft_labels[index] = uniform_vector
+
+                    
+    def __getitem__(self, index):
+        if self.mode == 'test':
+            img_path = self.test_imgs[index]
+            target=self.test_labels[img_path]
+            image=Image.open(img_path).convert('RGB')
+            img = self.transform(image)
+            return img, target
+            
+        elif self.mode == 'val':
+            img_path = self.val_imgs[index]
+            target=self.test_labels[img_path]
+            image=Image.open(img_path).convert('RGB')
+            img = self.transform(image)
+            return img, target
+            
+        elif self.mode == 'train_clean':
+            img_path = self.train_clean_imgs[index]
+            target=self.test_labels[img_path]
+            image=Image.open(img_path).convert('RGB')
+            img = self.transform(image)
+            return img, target
+
+        elif self.mode == 'nc_training':
+            img_path = self.train_imgs[index]
+            soft_target=self.soft_labels[index]
+            target=self.train_labels[img_path]
+            
+            image=Image.open(img_path).convert('RGB')
+            img = self.transform(image)
+            
+            if img_path in self.nc_imgs:
+                return img, soft_target, target+self.num_class
+            else:
+                tmp = np.zeros(self.soft_labels.shape[1],dtype=np.float32)  
+                tmp[np.argmax(soft_target)] = soft_target[np.argmax(soft_target)]
+                tmp = np.array(tmp,dtype=np.float32)
+            
+                return img, tmp, target   
+        
+
+    def __len__(self):
+        if self.mode == 'test':
+            return len(self.test_imgs)
+        elif self.mode == 'val':
+            return len(self.val_imgs)
+        elif self.mode == 'train_clean':
+            return len(self.train_clean_imgs)
+        elif self.mode == 'nc_training':
+            return len(self.train_imgs)
+
+        
+        
+class clothing_dataloader():
+    def __init__(self, root, batch_size, avg_prediction_vector=None, nc_set=None, num_workers=0):
+        
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.root = root
+        self.avg_prediction_vector = avg_prediction_vector
+        self.nc_set = nc_set
+        
+        self.transform_train = transforms.Compose([
+            transforms.Resize(256),
+            transforms.RandomCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.485, 0.456, 0.406),(0.229, 0.224, 0.225))
+        ])
+        self.transform_test = transforms.Compose([
+             transforms.Resize(256),
+             transforms.CenterCrop(224),
+             transforms.ToTensor(),
+             transforms.Normalize((0.485, 0.456, 0.406),(0.229, 0.224, 0.225))
+         
+         ])
+        
+        
+    def run(self, mode):
+        if mode == 'test':
+            test_dataset = clothing_dataset(self.root, transform=self.transform_test, mode='test')
+            test_loader = DataLoader(
+                dataset = test_dataset,
+                batch_size = self.batch_size,
+                shuffle = False,
+                num_workers=self.num_workers, drop_last=False)
+            return test_loader
+        elif mode == 'val':
+            val_dataset = clothing_dataset(self.root,transform=self.transform_test,mode='val')
+            val_loader = DataLoader(
+                dataset = val_dataset,
+                batch_size = self.batch_size,
+                shuffle = False,
+                num_workers = self.num_workers, drop_last=False)
+            return val_loader
+        elif mode == 'train_clean':
+            train_clean_dataset = clothing_dataset(self.root, transform=self.transform_train, mode='train_clean')
+            train_clean_loader = DataLoader(
+                dataset = train_clean_dataset,
+                batch_size = self.batch_size,
+                shuffle = True,
+                num_workers = self.num_workers, drop_last=False
+            )
+            return train_clean_loader
+        elif mode == 'train_val_mode':
+            train_v_dataset = clothing_dataset(self.root, transform=self.transform_test, mode='train')
+            train_v_loader = DataLoader(
+                dataset = train_v_dataset,
+                batch_size = self.batch_size,
+                shuffle = False,
+                num_workers = self.num_workers, drop_last=False
+            )
+            return train_v_loader, len(train_v_dataset)
+        
+        elif mode == 'nc_training':
+            train_nc_dataset = clothing_dataset(self.root, transform=self.transform_train, mode='nc_training', avg_prediction_vector=self.avg_prediction_vector, nc_set = self.nc_set)
+            train_nc_loader = DataLoader(
+                dataset = train_nc_dataset,
+                batch_size = self.batch_size,
+                shuffle = True,
+                num_workers = self.num_workers, drop_last=False
+            )
+
+            return train_nc_loader
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
